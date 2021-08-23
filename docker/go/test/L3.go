85e765594d2b6a52003ccc43b6e15f85e5b18380
hideru <jota.16.stigma.world@gmail.com>
2021-08-23 18:50:47 +0900

    L3 (Advanced)

--- docker/go/test/L3.go
+++ docker/go/test/L3.go
@@ -2,7 +2,24 @@ package main
 
 import "fmt"
 
+// If first charactor of var's Name is a capital, it can watch the var from other package.
+
 func main() {
-	var msg string
-	msg = ""
+	//var msg string
+	//msg = "Hello World!"
+	//var msg = "Hello World"
+	msg := "Hello World!!!!!!"
+	fmt.Println(msg)
+
+	//var a, b int
+	//a, b = 15, 10
+	a, b := 15, 10
+
+	var (
+		c int
+		d string
+	)
+	c = 10
+	d = "hoge"
+	fmt.Printf("a: %v, b: %v, c: %v, d: %v\n", a, b, c, d)
 }
